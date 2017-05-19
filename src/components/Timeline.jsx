838ec67837c196dfa1497678f8ec0cb34de8b401
KrickRay <krickray@gmail.com>
2017-05-19 15:56:36 +0300

    add timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -7,9 +7,85 @@ export default class Timeline extends React.Component {
 
     render() {
         return (
-            <div>
-
+            <div style={{display: 'table'}}>
+                {this.props.children}
             </div>
         );
+    }
+
+    static Line = class extends React.Component {
+        state = {};
+
+        static styles = {
+            line: {
+                display: 'table-row',
+            },
+            date: {
+                display: 'table-cell',
+                borderRight: '3px #e5e5d1 solid',
+                position: 'relative',
+                paddingTop: 7,
+                paddingRight: 15,
+                verticalAlign: 'top',
+                textTransform: 'uppercase',
+                textAlign: 'center',
+                minWidth: 170
+            },
+            dot: {
+                position: 'absolute',
+                right: -8,
+                top: 20,
+                width: 12,
+                height: 12,
+                backgroundColor: '#3c4854',
+                borderRadius: '100%'
+            },
+            content: {
+                display: 'table-cell',
+                padding: 10,
+                paddingRight: 0,
+                verticalAlign: 'top'
+            }
+        };
+
+        render() {
+            const {styles} = Timeline.Line;
+            const {locale, title, type, description, time, document, place} = this.props;
+            return (
+                <div style={styles.line}>
+                    <div style={styles.date}>
+                        <div style={{float: 'right'}}>
+                            {
+                                time[0].getFullYear() === time[1].getFullYear() ?
+                                    <div style={{float: 'left'}}>
+                                        <div>{time[0].toLocaleString(locale, {month: 'long'})}&nbsp;-&nbsp;{time[1].toLocaleString(locale, {month: 'long'})}</div>
+                                        <div style={{fontWeight: 600, fontSize: '22px', color: '#333'}}>{time[0].getFullYear()}</div>
+                                    </div>
+                                    :
+                                    <div>
+                                        <div style={{float: 'left'}}>
+                                            <div>{time[0].toLocaleString(locale, {month: 'long'})}</div>
+                                            <div style={{fontWeight: 600, fontSize: '22px', color: '#333'}}>{time[0].getFullYear()}</div>
+                                        </div>
+                                        <div style={{float: 'left'}}>
+                                            <div>&nbsp;-&nbsp;</div>
+                                        </div>
+                                        <div style={{float: 'left'}}>
+                                            <div>{time[1].toLocaleString(locale, {month: 'long'})}</div>
+                                            <div style={{fontWeight: 600, fontSize: '22px', color: '#333'}}>{time[1].getFullYear()}</div>
+                                        </div>
+                                    </div>
+                            }
+                            <div style={styles.dot}/>
+                        </div>
+                    </div>
+                    <div style={styles.content}>
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                        <p><b>{place.town}</b>, <i>{place.department} {place.name}</i></p>
+                    </div>
+                </div>
+            );
+        }
     }
 }
--- src/resume/arsaev.js
+++ src/resume/arsaev.js
@@ -3,6 +3,7 @@ export default {
         name: 'Арсаев Ринат Вадимович',
         title: 'Full-stack JavaScript разработчик',
         phoneNumber: '+7(981)828-45-16',
+        email: 'krickray@gmail.com',
         birthday: new Date(1993, 11, 20),
         social: {
             skype: '',
@@ -40,6 +41,7 @@ export default {
                 'PostCSS',
                 'Stylus',
                 'HTML',
+                'Веб-компоненты'
             ],
             'Базы данных': [
                 'MongoDB',
@@ -51,12 +53,156 @@ export default {
                 'Adobe Photoshop'
             ]
         },
-        education: [],
-        jobs: []
+        timeline: [
+            // {
+            //     title: 'Среднее общее образование',
+            //     type: 'education',
+            //     time: [new Date(2001, 8, 1), new Date(2012, 5, 23)],
+            //     document: 'Аттестат',
+            //     place: {
+            //         name: 'МОБУ "Башкирская гимназия"',
+            //         department: 'Физико-математический класс',
+            //         town: 'Агидель',
+            //     }
+            // },
+            {
+                title: 'Высшее образование',
+                type: 'education',
+                description: 'Бакалавр по специальности программная инжененрия',
+                time: [new Date(2012, 8, 1), new Date(2016, 5, 23)],
+                document: 'Диплом бакалавра по специальности "Программная инженерия"',
+                place: {
+                    name: 'ФГБОУ ВО «Уфимский государственный авиационный технический университет»',
+                    department: 'Факультет информатики и робототехники',
+                    town: 'Уфа',
+                }
+            }, {
+                title: 'Сервис-менеджер',
+                type: 'job',
+                time: [new Date(2013, 6, 22), new Date(2013, 8, 17)],
+                description: 'Решение проблем с интернетом и телевидением у клиентов. Продажи',
+                place: {
+                    department: 'Отдел продаж',
+                    town: 'Уфа',
+                    name: 'ОАО «Уфанет»'
+                }
+            }, {
+                title: 'Frontend разработчик',
+                type: 'job',
+                time: [new Date(2015, 6, 1), new Date(2015, 7, 31)],
+                description: 'Разработка модуля корпоративной социальной сети',
+                place: {
+                    management: 'Управления разработки',
+                    department: 'Отдел разработки',
+                    name: 'ЗАО «ПФ «СКБ Контур»',
+                    town: 'Екатеринбург',
+                }
+            }, {
+                title: 'Оператор научной роты',
+                type: 'education',
+                time: [new Date(2016, 6, 7), new Date(2017, 6, 6)],
+                description: 'Разработка веб-сервиса мониторинга сетевого оборудования',
+                place: {
+                    town: 'Санкт-Петербург',
+                    department: 'Научно-исследовательский центр',
+                    name: 'Военной академии связи имени С.М. Будённого',
+                }
+            }
+        ]
     },
     en: {
         name: 'Rinat Arsaev',
         title: 'Full-stack JavaScript developer',
-        location: 'Saint Petersburg'
+        location: 'St. Petersburg',
+        phoneNumber: '+7(981)828-45-16',
+        email: 'krickray@gmail.com',
+        birthday: new Date(1993, 11, 20),
+        description: '',
+        skills: {
+            'Code': [
+                'JavaScript',
+                'ES2015',
+                'ES6',
+                'ES7',
+                'Node.js',
+                'React',
+                'Redux',
+                'Arduino',
+                'Java',
+                'C#',
+                'C++',
+                'C',
+                'PHP'
+            ],
+            'Tools': [
+                'Webpack',
+                'Gulp',
+                'Git',
+                'GitHub',
+                'NPM'
+            ],
+            'Layout': [
+                'CSS',
+                'BEM',
+                'SCSS',
+                'PostCSS',
+                'Stylus',
+                'HTML',
+                'Web components'
+            ],
+            'Database': [
+                'MongoDB',
+                'SQL',
+                'MySQL',
+            ],
+            'Programs': [
+                'JetBrains WebStorm',
+                'Adobe Photoshop'
+            ]
+        },
+        timeline: [
+            {
+                title: 'Higher education',
+                type: 'education',
+                description: 'Software Engineering bachelor',
+                time: [new Date(2012, 8, 1), new Date(2016, 5, 23)],
+                document: 'Диплом бакалавра по специальности "Программная инженерия"',
+                place: {
+                    name: 'Ufa State Aviation Technical University',
+                    department: 'Faculty of Informatics and Robotics',
+                    town: 'Ufa',
+                }
+            }, {
+                title: 'Service manager',
+                type: 'job',
+                time: [new Date(2013, 6, 22), new Date(2013, 8, 17)],
+                description: 'Solving customer problems with the Internet and television. Sales',
+                place: {
+                    department: 'Sales department',
+                    town: 'Ufa',
+                    name: 'Ufanet'
+                }
+            }, {
+                title: 'Frontend developer',
+                type: 'job',
+                time: [new Date(2015, 6, 1), new Date(2015, 7, 31)],
+                description: 'Development of corporate social network module',
+                place: {
+                    department: 'Development department',
+                    name: 'SKB Kontur',
+                    town: 'Ekaterinburg',
+                }
+            }, {
+                title: 'Operator of a scientific company',
+                type: 'education',
+                time: [new Date(2016, 6, 7), new Date(2017, 6, 6)],
+                description: 'Development of a web-based monitoring service for network equipment',
+                place: {
+                    town: 'St. Petersburg',
+                    department: 'Research Center',
+                    name: 'Budyonny Military Academy of the Signal Corps',
+                }
+            }
+        ]
     }
 }
